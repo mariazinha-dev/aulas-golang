bbb9a31a61274835d5ce2cb51336a986326a8f36
Aluno Dev25 <maria.e.guimaraes20@aluno.senai.br>
2025-03-28 09:19:54 -0300

    desafio entrada de dados e reconhecimento se está correto

--- main.go
+++ main.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var num1 int
-	var num2 int
-	fmt.Print("digite um numero: ")
- 	fmt.Scan(&num1)
-	 fmt.Println("o numero digitado", num1)
-	 fmt.Print("digite um numero: ")
- 	fmt.Scan(&num2)
-	 fmt.Println("o numero digitado", num2)
-	 fmt.Println("a soma é: ", num1 + num2)
- 	fmt.Println("A multiplicação é: ", num1 * num2)
- 	fmt.Println("A divisão é: ",num1 / num2)
- 	fmt.Println("subtração é  ", num1 - num2)
- 	fmt.Println("O resto da divisão é ",num1 % num2)
- 	
+var admin string
+var senha int
+fmt.Print("Digite seu usuário: ")
+fmt.Scan(&admin)
+fmt.Println("o usuário é: ", admin)
+fmt.Print("digite sua senha: ")
+fmt.Scan(&senha)
+fmt.Println("a senha é: ", senha)
+if admin == "admin" &&  senha == 1234{
+	fmt.Print("Acesso permitido! seja bem vindo")
+}else {
+	fmt.Printf("acesso negado, tente novamente")
+}
+
  }
  	
  
