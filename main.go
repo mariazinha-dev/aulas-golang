67cf169637d298f89dcb913d1d5bce5abaada825
Aluno Dev25 <maria.e.guimaraes20@aluno.senai.br>
2025-04-04 09:23:00 -0300

    index,value

--- main.go
+++ main.go
@@ -20,4 +20,26 @@ names:=[]string {"Carol","Maicon", "ana"}
 sort.Strings(names)
 fmt.Println(names)
 fmt.Println(sort.SearchStrings(names,"Carol"))
+x:=15
+
+for x<5 {
+	fmt.Println(x)
+	x++
+}
+
+for i:=0; i <5; i++{
+	fmt.Println("for 2: ", i)
+}
+
+for i:=0;  i<len(names); i++ {
+	fmt.Println(names[i])
+}
+
+for index, value := range names{
+fmt.Println("o índice é: ", index, "e o valor é: ", value)
+}
+
+for index, value := range ages{
+fmt.Println("o índice é: ", index, "e o valor é: ", value)
+}
 }
