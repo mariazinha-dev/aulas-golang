56bfc52de5ff10059d9cdf45d374e821215b9692
Aluno Dev25 <maria.e.guimaraes20@aluno.senai.br>
2025-04-10 13:06:55 -0300

    desafio 2

--- main.go
+++ main.go
@@ -3,14 +3,36 @@ package main
  import "fmt"
  
  func main() {
-	var idade int
-	fmt.Println("digite sua idade: ")
-	fmt.Scan(&idade)
-	if idade <18 {
-		fmt.Println("você é de menor, não pode entrar!")
-	} else if idade >=18 && idade <=60{
-		fmt.Println("você é de maior, pode trar meu rapaz/senhorita/monsieur")
-	} else {
-		fmt.Println("você é um idoso!")
-	}
- }
+var saldo int
+var opcao string
+var deposito int
+var saque int
+fmt.Println("digite seu saldo")
+fmt.Scan(&saldo)
+for {
+fmt.Println("voce quer 'sacar' ou 'depositar'?")
+fmt.Scan(&opcao)
+if opcao == "sacar"{
+fmt.Println("quanto você deseja sacar?")
+fmt.Scan(&saque)
+fmt.Println("seu saldo é: ")
+fmt.Scan("&saque")
+saldo = saldo - saque
+fmt.Println(saldo)
+}
+if opcao == "depositar"{
+fmt.Println("quanto vc quer depositar?")
+fmt.Scan(&deposito)
+fmt.Println("seu saldo é:")
+fmt.Scan("&deposito")
+saldo = saldo + deposito
+fmt.Println(saldo)
+}
+fmt.Println("Você deseja fazer mais uma transação? s/n")
+var opcao1 string
+fmt.Scan(&opcao1)
+if opcao1 == "n" {
+	break
+}
+}
+}
