8e2cd609e8de2ef720e19afb7e0842d6fe50826a
Aluno Dev25 <maria.e.guimaraes20@aluno.senai.br>
2025-04-10 10:26:47 -0300

    programação idade desafio

--- main.go
+++ main.go
@@ -3,31 +3,14 @@ package main
  import "fmt"
  
  func main() {
-	age := 45
- fmt.Println(age <=50)
- fmt.Println(age >=50)
- fmt.Println(age ==50)
- fmt.Println(age !=50)
-
- if age <50{
-	fmt.Println("menor que 30 anos")
- } else if age <40{
-	fmt.Println("menor que 40 anos") 
- }else{
-	fmt.Println("não é menos que 40 anos")
- }
-names:=[]string{"Isadora","Yasmim","Trunks","Cebolinha", "Martin","Yuri"}
-
-for index, value :=range names{
-	if index == 1{
-		fmt.Println("continua após a posição", index,"e value",value)
-		continue
+	var idade int
+	fmt.Println("digite sua idade: ")
+	fmt.Scan(&idade)
+	if idade <18 {
+		fmt.Println("você é de menor, não pode entrar!")
+	} else if idade >=18 && idade <=60{
+		fmt.Println("você é de maior, pode trar meu rapaz/senhorita/monsieur")
+	} else {
+		fmt.Println("você é um idoso!")
 	}
-if index >2{
-	fmt.Println("sair após", index)
-	break 
-}
-fmt.Println("valor: ", value)
- }
-
  }
