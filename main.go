5229cb2c4ed86f1e5d1d48f71173a28b68b0bc8b
Aluno Dev25 <maria.e.guimaraes20@aluno.senai.br>
2025-04-10 08:23:03 -0300

    numeros inteiros com soma

--- main.go
+++ main.go
@@ -1,45 +1,22 @@
 package main
+ 
+ import "fmt"
+ 
+ func main() {
+ var numeros[5]int
+ var soma int
 
-import (
-"fmt"
-"strings"
-"sort"
-)
-func main () {
-greeting := "Hello my friends"
-fmt.Println(strings.Contains(greeting,"dog"))
-fmt.Println(strings.ReplaceAll(greeting,"my", "mine"))
-fmt.Println(strings.ToUpper(greeting))
-fmt.Println(strings.Index(greeting,"my"))
-ages:=[]int {50,80,10,}
-sort.Ints(ages)
-fmt.Println(ages)
-index := sort.SearchInts(ages,50)
-fmt.Println(index)
-names:=[]string {"Carol","Maicon", "ana"}
-sort.Strings(names)
-fmt.Println(names)
-fmt.Println(sort.SearchStrings(names,"Carol"))
-x:=15
+ fmt.Print("digite 5 numeros inteiros:")
+ 
 
-for x<5 {
-	fmt.Println(x)
-	x++
-}
+ for i := 0; i < 5; i++ {
+	fmt.Printf("Número %d: ", i+1)
+	fmt.Scan(&numeros[i])
+	soma += numeros[i]
 
-for i:=0; i <5; i++{
-	fmt.Println("for 2: ", i)
-}
+ }
 
-for i:=0;  i<len(names); i++ {
-	fmt.Println(names[i])
-}
+fmt.Printf("a soma dos numeros é: %d\n", soma)
 
-for index, value := range names{
-fmt.Println("o índice é: ", index, "e o valor é: ", value)
-}
-
-for index, value := range ages{
-fmt.Println("o índice é: ", index, "e o valor é: ", value)
-}
-}
+ 
+  }
