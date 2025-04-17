f0e1373ca5546074b60bf5abb6b76af72e8d23cb
Aluno Dev25 <maria.e.guimaraes20@aluno.senai.br>
2025-04-17 10:57:19 -0300

    nomes e idades 17/04

--- main.go
+++ main.go
@@ -3,20 +3,21 @@ package main
  import (
 	"fmt")
 
-func dadosPessoa(idade int) (int, string){
-var condicao string
-if idade >= 18 {
-	condicao = "você é maior de idade"
-} else {
-	condicao = "você é menor de idade"
-}
-return idade, condicao
-}
 
-func main(){
-	var idadeusuario int
-fmt.Println("qual sua idade?")
-fmt.Scan(&idadeusuario)
-idade, condicao := dadosPessoa(idadeusuario)
-fmt.Println("você tem", idade, "anos e", condicao)
+func main (){
+	alunoIdade := make(map[string]int)
+	alunoIdade["carol"] =15 
+	alunoIdade["maria"] = 15
+	alunoIdade["aninha"] = 16
+	alunoIdade["Matheus"] = 16
+	fmt.Println("idade da carolzinha",alunoIdade["carol"])
+	notasAlunos := map[string]float64{
+		"carol" : 5.5,
+		"maria" : 10,
+		"aninha" : 7.5,
+		"matheus":7.5,
+	}
+	for nome,nota := range notasAlunos{
+	fmt.Printf("%s tirou a nota %f\n",nome,nota)
+	}
 }
