6d3c7955a214286706440c3af74a0ff1719adb0f
Aluno Dev25 <maria.e.guimaraes20@aluno.senai.br>
2025-04-24 14:16:18 -0300

    notas aprovado/Reprovado

--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
- 
- import (
-	"fmt")
 
+import (
+	"fmt"
+)
 
-func main (){
-	alunoIdade := make(map[string]int)
-	alunoIdade["carol"] =15 
-	alunoIdade["maria"] = 15
-	alunoIdade["aninha"] = 16
-	alunoIdade["Matheus"] = 16
-	fmt.Println("idade da carolzinha",alunoIdade["carol"])
-	notasAlunos := map[string]float64{
-		"carol" : 5.5,
-		"maria" : 10,
-		"aninha" : 7.5,
-		"matheus":7.5,
-	}
-	for nome,nota := range notasAlunos{
-	fmt.Printf("%s tirou a nota %f\n",nome,nota)
+func mediaNotas(n1, n2 float64) (float64, string){
+
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return media, "aprovado"
+	} else
+	{
+		return media, "reprovado"
 	}
 }
+func main() {
+	media, resultado := mediaNotas(9.5, 8.8)
+	fmt.Println("Média:",media)
+	fmt.Println("Resultado:", resultado)
+}
+
+
+
+	
+
+ 
+
+
+ 
